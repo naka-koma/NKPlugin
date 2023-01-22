52501de49faf574b99f26b5758fd0e233154d418
NakaKoma <nakakomadev@gmail.com>
2023-01-22 17:16:04 +0900

    確認用のスクリプト追加

---
+++ Assets/NKPlugin/Runtime/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NKPlugin.Runtime.Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        /// <summary>
+        /// <see cref="Start"/> is called on the frame when a script is enabled just before any of the <seealso cref="Update"/> methods are called the first time.
+        /// </summary>
+        private void Start()
+        {
+            
+        }
+
+        /// <summary>
+        /// <see cref="Update"/> is called every frame, if the <seealso cref="MonoBehaviour"/> is enabled.
+        /// </summary>
+        private void Update()
+        {
+
+        }
+    }
+}
